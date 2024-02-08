9fcdb8eac75e7e376c64ebe5715adf124b66adbf
Maverick Wong <mvrck.wong@gmail.com>
2024-02-08 15:10:07 +0800

    update

--- src/01_Extraction.py
+++ src/01_Extraction.py
@@ -8,7 +8,6 @@ from google.cloud import bigquery
 from typing import List
 from dataset import ecommerce_dataset
 
-
 # General Directories
 SRC_DIR = Path(__file__).parent
 PROJECT_DIR = SRC_DIR.parent
@@ -27,24 +26,32 @@ environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(BIGQUERY_JSON_FILE)
 def query_to_df(bigquery_dataset:str) -> pd.DataFrame.to_csv:
     """ Query the data from bigquery and save it to pandas dataframe."""
     client = bigquery.Client()
-    sql_query = f"SELECT * FROM `{bigquery_dataset}`"
+    sql_query = f"""
+        SELECT * FROM `{bigquery_dataset}`
+    """
     return client.query(sql_query).to_dataframe()
 
 @logger.catch
-def main(link_datasets:List[str]):
+def main(link_datasets:List[str], test=False):
     # Loop through the Bigquery Dataset selected
-    for dataset in link_datasets:
+    for index, dataset in enumerate(link_datasets):
         name_list = dataset.split(".")[-2:]
         name = "-".join(name_list) + ".csv"
         
+        # Query and Save the data
         df = query_to_df(dataset)
-        df.to_csv(DATA_DIR / name, dtype=str)
+        df.to_csv(DATA_DIR / name)
+        
+        if index==0 and test==True: 
+            break
+        
     return True
 
 if __name__ == "__main__":
     # Setting the current dataset
     current_dataset = ecommerce_dataset
+    debug_stage = False
     
     # Run the main function
-    if main(current_dataset):
+    if main(current_dataset, test=debug_stage):
         logger.info("Main function successfully executed.")
